53a80774f380a9bee1c5936fe95d05ea4ba8629a
James Cutler <jamescutler1111@gmail.com>
2019-04-05 10:37:44 -0400

    fixed js overwrite

--- rps.js
+++ rps.js
@@ -1,77 +1,59 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Rock Paper Scissors</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css"> -->
-    <script src="rps.js"></script>
-    <style>
-        .choice img {
-            max-width: 200px;
-            height: auto;
-            float: none;
-            display: block;
-            margin: 0 auto;
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //do work
+    var choices = [
+        {
+            "name":"rock",
+            "beats":"scissors"
+        },
+        {
+            "name":"scissors",
+            "beats":"paper"
+        },
+        {
+            "name":"paper",
+            "beats":"rock"
         }
-        .choice img:hover {
-            cursor: crosshair;
+    ];
+    var randomNum;
+    var computerChoice;
+    var userChoice;
+    var gameOver = false;
+    function whoWins(computerChoice, userChoice){
+        if (computerChoice.beats == userChoice){
+            return "Failure. The machines win."
+        } else if (computerChoice.name == userChoice) {
+            return "You fought well. Now dust yourself off - tomorrow's another day."
+        } else {
+            return "Victory! The humans have beaten the machines. Thank you for your hard work."
         }
-        .page-center {
-            max-width: 900px;
-            margin: 0 auto;
-        }
-        .choices {
-            /* height: 200px; */
-            display: flex;
-        }
-        .choice {
-            flex-grow: 1;
-        }
-        #result {
-            margin-top: 100px;
-            text-align: center;
-            opacity: 0;
-        }
-        #playAgain {
-            opacity: 0;
-            /* display: none; */
-            text-align: center;
-        }
-        * {
-            transition: opacity 2s ease;
-        }
-    </style>
-</head>
-<body>
-    <div class="page-center">
-        <div class="choices">
-            <div class="choice">
-                <img  data="rock" src="https://jooinn.com/images450_/rock-1.jpg">
-            </div>
+    }
+    function playAgain(){
+        document.querySelector('.choices').setAttribute('style','opacity: 1');
+        document.getElementById('playAgain').setAttribute('style','opacity: 0');
+        document.getElementById('result').setAttribute('style','opacity: 0;');
+    }
+    var userChoices = document.querySelectorAll('.choice img');
+    for (i=0;i<userChoices.length;i++) {
+        userChoices[i].addEventListener('click', function(){
+            if (!gameOver) {
+                randomNum = Math.floor(Math.random() * Math.floor(3));
+                computerChoice = choices[randomNum];
+                console.log("computer choice: " + computerChoice.name);
+                userChoice = this.getAttribute('data');
+                console.log('user choice: ' + userChoice);
+                document.getElementById('result').innerHTML = whoWins(computerChoice, userChoice);
+                // console.log(document.querySelectorAll('.choice img')[0].setAttribute('style','display: none'));
+                document.querySelector('.choices').setAttribute('style','opacity: 0');
+                document.getElementById('result').setAttribute('style','opacity: 1');
+                document.getElementById('playAgain').setAttribute('style','opacity: 1;');
+
+            }
             
-            <br>
-            <div class="choice">
-                    <img  data="paper" src="https://image.freepik.com/free-photo/top-view-pieces-paper-wooden-table_1232-744.jpg">
-            </div>
-            
-            <br>
-            <div class="choice">
-                    <img  data="scissors" src="https://cdn.pixabay.com/photo/2016/03/31/23/11/scissors-1297454__340.png">
-            </div>
-            
-            <br>
-            
-        </div>
-    </div>
-    <div class="page-center">
-        <div id="result"></div>
-        <div id="playAgain">
-            <p>
-                <a href="javascript:;">Play Again?</a>
-            </p>
-        </div>
-    </div>
-</body>
-</html>
+        });
+    }
+    document.querySelector('#playAgain a').addEventListener('click',function(){
+        playAgain();
+    });
+    
+  });
